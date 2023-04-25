cc361974c041e8be6ae84aec4a026e443ee10fe6
Juan Pais <jjosepais@gmail.com>
2023-04-24 22:05:06 -0300

    saco Drawer. Pongo imagen de fondo.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,50 @@
-import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	ImageBackground,
+} from "react-native";
 import {useNavigation} from "@react-navigation/native";
+
+const image = {
+	uri: "https://as1.ftcdn.net/v2/jpg/01/51/46/38/1000_F_151463899_Ab08hKzTS1PvBifzl4hmWgPmuvcy2RC2.jpg",
+};
 
 const HomeScreen = () => {
 	const navigation = useNavigation();
 	return (
-		<View>
-			<Text
+		<View style={{flex: 1, flexDirection: "column"}}>
+			<ImageBackground
+				source={image}
 				style={{
-					fontSize: 30,
-					textAlign: "center",
-					marginTop: "20%",
+					flex: 1,
+					resizeMode: "cover",
+					justifyContent: "center",
+					alignItems: "center",
+					alignContent: "center",
 				}}
 			>
-				Home Screen
-			</Text>
+				<Text
+					style={{
+						fontSize: 30,
+						textAlign: "center",
+						marginTop: "20%",
+					}}
+				>
+					Home Screen
+				</Text>
 
-			<TouchableOpacity onPress={() => navigation.navigate("Stack")}>
-				<Text>Te llevo al StackScreen</Text>
-			</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => navigation.navigate("Stack")}
+					style={{
+						justifyContent: "center",
+						backgroundColor: "white",
+					}}
+				>
+					<Text>Te llevo al StackScreen</Text>
+				</TouchableOpacity>
+			</ImageBackground>
 		</View>
 	);
 };
